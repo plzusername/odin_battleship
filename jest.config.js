98dfaa235abd8f039f3c50e65630c129ee9815a3
plzusername <MohamedFaris2210@gmail.com>
2024-06-13 11:23:56 +0300

    Change jest env to dom

--- jest.config.js
+++ jest.config.js
@@ -145,7 +145,8 @@ const config = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
+
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
