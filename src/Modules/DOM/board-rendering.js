827fe654922063a126d50f8ccb46813adb597f6d
plzusername <MohamedFaris2210@gmail.com>
2024-09-18 19:54:00 +0300

    Add logic to style sunken and neighboring squares

--- src/Modules/DOM/board-rendering.js
+++ src/Modules/DOM/board-rendering.js
@@ -35,57 +35,15 @@ const applyShipPresentStyles = (domCell) => {
   domCell.appendChild(battleShip);
 };
 
-function getNeighboringSquares(cellIndex, shipGameBoard) {
-  let neighboringSquares = [];
-
-  shipGameBoard.shipLocations.forEach((shipLocation) => {
-    if (shipLocation.occupiedSquares.includes(cellIndex)) {
-      shipLocation.occupiedSquares.forEach((occupiedSquare) => {
-        neighboringSquares.push(...shipGameBoard.get_neighbors(occupiedSquare));
-      });
-    }
-  });
-
-  neighboringSquares = neighboringSquares.filter(
-    (square, index, array) => index == array.indexOf(square)
-  );
-
-  return neighboringSquares;
+function getIsNeighborSquare(coordinates, gameBoard) {
+  return gameBoard.returnCoordinatesWereNeighbor(coordinates);
 }
 
-function getOccupiedSquares(neighboringSquares, shipGameBoard) {
-  const occupied_squares = [];
-
-  neighboringSquares.forEach((neighboringSquare) => {
-    if (returnCoordinatesAreShip(neighboringSquare)) {
-      occupied_squares.push(neighboringSquare);
-    }
-  });
-
-  return occupied_squares;
-}
-
-const getSunkenNeighborSquares = (shipGameBoard, domCell) => {
-  const domCellIndex = +domCell.dataset.coordinates;
-  const neighboringSquares = getNeighboringSquares(domCellIndex, shipGameBoard);
-  const occupiedSquares = getOccupiedSquares(neighboringSquares, shipGameBoard);
-
-  return { neighboringSquares, occupiedSquares };
+const applyNeighborStyles = (domCell) => {
+  domCell.classList.add("neighboring-square");
 };
 
-const applyNeighborStyles = (domCell, neighborSquareIndex) => {
-  const neighboringDomCell = domCell.closest(
-    `[data-coordinates=${neighborSquareIndex}]`
-  );
-
-  neighboringDomCell.classList.add("empty-cell-hit");
-};
-
-const applyOccupiedStyles = (domCell, occupiedSquareIndex) => {
-  const occupiedDomCell = domCell.closest(
-    `[data-coordinates=${occupiedSquareIndex}]`
-  );
-
+const applyOccupiedStyles = (domCell) => {
   const sunkenShip = createElement(
     "img",
     {
@@ -96,19 +54,8 @@ const applyOccupiedStyles = (domCell, occupiedSquareIndex) => {
     ""
   );
 
-  occupiedDomCell.appendChild(sunkenShip);
-};
-
-const applySunkenShipStyles = (shipGameBoard, domCell) => {
-  const squaresToBeStyled = getSunkenNeighborSquares(shipGameBoard, domCell);
-
-  squaresToBeStyled.neighboringSquares.forEach((neighboringSquare) => {
-    applyNeighborStyles(domCell, neighboringSquare);
-  });
-
-  squaresToBeStyled.occupiedSquares.forEach((occupiedSquare) => {
-    applyOccupiedStyles(domCell, occupiedSquare);
-  });
+  domCell.classList.add("occupied-square");
+  domCell.appendChild(sunkenShip);
 };
 
 function applyHitCellStyles(cell, domCell) {
@@ -124,10 +71,15 @@ function applyNonShipCellStyles(cell, domCell, gameBoard) {
   const coordinates = +domCell.dataset.coordinates;
 
   if (
-    gameBoard.returnCoordinatesAreShip(coordinates) &&
+    isObject(gameBoard.returnPreviousShipAtCoords(coordinates)) &&
     gameBoard.shipIsSunken(coordinates)
   ) {
-    applySunkenShipStyles(gameBoard, domCell);
+    applyOccupiedStyles(domCell);
+    return;
+  }
+
+  if (getIsNeighborSquare(coordinates, gameBoard)) {
+    applyNeighborStyles(domCell);
     return;
   }
 
@@ -164,32 +116,30 @@ function translateIndeciesToBoard(shipLocations, hitSquares, missSquares) {
   return board;
 }
 
+function resetCell(domBoardCell) {
+  const originClass = domBoardCell.classList.value.split(" ")[0];
+
+  domBoardCell.removeAttribute("class");
+  domBoardCell.classList.add(originClass);
+
+  domBoardCell.replaceChildren();
+}
+
 function renderGameboard(Board, domBoardCells, gameBoard) {
-  domBoardCells.forEach((domBoardCell) => {
-    const originClass = domBoardCell.classList.value.split(" ")[0];
-
-    domBoardCell.removeAttribute("class");
-    domBoardCell.classList.add(originClass);
-
-    domBoardCell.replaceChildren();
-  });
   Board.forEach((cell, index) => {
-    applyCellStyles(cell, domBoardCells[index], gameBoard);
+    const domCell = domBoardCells[index];
+
+    resetCell(domCell);
+    applyCellStyles(cell, domCell, gameBoard);
   });
 }
 
 function renderComputerGameBoard(Board, domBoardCells, gameBoard) {
-  domBoardCells.forEach((domBoardCell) => {
-    const originClass = domBoardCell.classList.value.split(" ")[0];
-
-    domBoardCell.removeAttribute("class");
-    domBoardCell.classList.add(originClass);
-
-    domBoardCell.replaceChildren();
-  });
-
   Board.forEach((cell, index) => {
-    applyNonShipCellStyles(cell, domBoardCells[index], gameBoard);
+    const domCell = domBoardCells[index];
+
+    resetCell(domCell);
+    applyNonShipCellStyles(cell, domCell, gameBoard);
   });
 }
 
